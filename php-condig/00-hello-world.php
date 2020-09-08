86d0263e9a8cdb5dc12503d601d7da4de171f331
Tur1st4 <paulo.eduardo.m.jacob@gmail.com>
2020-09-08 10:39:38 -0300

    Adicionado o Hello World

---
+++ php-condig/00-hello-world.php
@@ -0,0 +1,3 @@
+<?php // Essa primeira linha e a última são obrigatórias em todos os seus programas em PHP. Obs: A última nem sempre
+    echo "Olá, mundo!\n"; // O comando 'echo' escreve na tela tudo que está entre aspas, o '\n' serve para pular uma linha 
+?>
